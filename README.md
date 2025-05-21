5c7e396423409d260c0e6c8ae3ce817ffcf0e0f1
Kristina Kudryavceva <inbox_es@mail.ru>
2025-05-21 14:03:51 +0300

    project is complete

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/kristinakpl/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+// xtodo: Темплейт карточки
 
-// @todo: DOM узлы
+// xtodo: DOM узлы
 
-// @todo: Функция создания карточки
+// xtodo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+// xtodo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+// xtodo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function handleCardDelete(cardElement) {
+  cardElement.remove();
+}
+
+function createCard(item, deleteCallback) {
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = item.link.trim();
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+
+  deleteButton.addEventListener("click", function () {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+initialCards.forEach(function (item) {
+  const cardElement = createCard(item, handleCardDelete);
+  placesList.append(cardElement);
+});
